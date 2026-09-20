9f0446d4d429ef6f74b05147710df85a6aae7a23
Lev <tolkoplus@gmail.com>
2026-09-20 15:17:46 +0000

    Показывать задачи при выборе темы физики

--- server.js
+++ server.js
@@ -1853,7 +1853,7 @@ function decorate(html, section, onlyAdded = null, seoOverride = null) {
         return /кредит|банк|вклад|заём|долг|плат[её]ж|процентн.{0,20}ставк|ценн.{0,10}бумаг|пенсионн.{0,10}фонд/.test(text);
       }
       if (section === 'physics' && physicsTopic) {
-        const code = meta.match(/\b([1-5]\.[0-9]+(?:\.[0-9]+)*)\b/)?.[1] || '';
+        const code = (meta + ' ' + text).match(/\b([1-5]\.[0-9]+(?:\.[0-9]+)*)\b/)?.[1] || '';
         return code === physicsTopic || code.startsWith(physicsTopic + '.');
       }
       if (section === 'planimetry') {
